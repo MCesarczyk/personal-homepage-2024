71ae14f624a6c1e6b8fd742ce3273b2c264df391
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-02-25 00:00:57 +0100

    refactor(button): externalize filesave utility function

---
+++ src/common/utils.ts
@@ -0,0 +1,7 @@
+import JSZipUtils from "jszip-utils";
+import saveAs from "save-as";
+
+export const saveFileFromUrlWithName = async (url: string, name: string) => {
+  const buffer = await JSZipUtils.getBinaryContent(url);
+  return saveAs(new Blob([buffer]), name);
+};
--- src/ui/atoms/button/Button.stories.tsx
+++ src/ui/atoms/button/Button.stories.tsx
@@ -2,19 +2,11 @@
 
 import type { Meta, StoryFn } from "@storybook/react";
 import { type ComponentProps } from "react";
-import JSZipUtils from "jszip-utils";
-import saveAs from "save-as";
 
 import { Button } from "./Button";
+import { saveFileFromUrlWithName } from "../../../common/utils";
 
-const saveFileFromUrlWithName = async (url: string, name: string) => {
-  const buffer = await JSZipUtils.getBinaryContent(url);
-  return saveAs(new Blob([buffer]), name);
-};
-
-const handleClick = () =>
-  // saveFileFromUrlWithName("https://css4.pub/2015/icelandic/dictionary.pdf", "Dictionary of Iceland.pdf");
-  saveFileFromUrlWithName("/sample.pdf", "Dictionary of Iceland.pdf");
+const handleClick = () => saveFileFromUrlWithName("/sample.pdf", "Blue sky pinting.pdf");
 
 const meta: Meta<typeof Button> = {
   component: Button,
