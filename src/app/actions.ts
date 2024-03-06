b346e70f06af894d4f74733252c5f21720150775
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-06 20:27:39 +0100

    feat(admin): set cookie on login, use in admin page to authenticate

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,20 +1,26 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
 export const handleLogin = async (formData: FormData) => {
   "use server";
 
   const email = formData.get("email") as string;
   const password = formData.get("password") as string;
 
-  console.log(`Logging in as ${email} with password ${password} to ${process.env.BACKEND_URL}`);
+  console.log(`Logging in as ${email} with password ${password} to ${process.env.API_URL}`);
 
   const response = await fetch(`${process.env.API_URL}${process.env.API_PREFIX}/auth/login`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
     },
+    credentials: "include",
     body: JSON.stringify({ email, password }),
   });
 
   const data = await response.json();
 
-  console.log(data);
+  cookies().set("access_token", data.accessToken);
+
+  redirect("/admin");
 };
--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,23 @@
+import { cookies } from "next/headers";
 import { PageTitle } from "@/ui";
 
 export default async function Admin() {
-  return <PageTitle>Admin panel</PageTitle>;
+  const accessToken = cookies().get("access_token")?.value;
+
+  const user = await fetch(`${process.env.API_URL}${process.env.API_PREFIX}/user/profile`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    credentials: "include",
+  });
+  const data = await user.json();
+
+  return (
+    <>
+      <PageTitle>Admin panel</PageTitle>
+      <p>Welcome, {data.name}!</p>
+    </>
+  );
 }
