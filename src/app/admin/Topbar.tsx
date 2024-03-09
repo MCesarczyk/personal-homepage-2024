f748eb8bf0ed91bfce7e01f7ec74928344b2e7ae
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-09 19:36:19 +0100

    feat(topbar): create logout action

--- src/app/admin/Topbar.tsx
+++ src/app/admin/Topbar.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import React, { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 import Cookies from "js-cookie";
 
 import { Button } from "@/ui";
 
 export const Topbar = () => {
   const accessToken = Cookies.get("accessToken");
+  const router = useRouter();
   const [username, setUsername] = useState("");
 
   const getUserData = async () => {
@@ -41,6 +43,25 @@ export const Topbar = () => {
     console.log(data.accessToken);
   };
 
+  const logout = async () => {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_API_URL}${process.env.NEXT_PUBLIC_API_PREFIX}/auth/logout`,
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: `Bearer ${accessToken}`,
+        },
+        credentials: "include",
+      },
+    );
+
+    if (response.ok) {
+      Cookies.remove("accessToken");
+      router.push("/login");
+    }
+  };
+
   useEffect(() => {
     getUserData();
   }, []); // eslint-disable-line react-hooks/exhaustive-deps
@@ -48,6 +69,7 @@ export const Topbar = () => {
   return (
     <div className="w-2/3 md:w-1/2 mt-[-48px] mb-16 mr-auto flex flex-col sm:flex-row items-center gap-6 z-20">
       <p className="h-full">{username}</p>
+      <Button onClick={logout}>Log out</Button>
       <Button onClick={refreshToken}>Refresh token</Button>
     </div>
   );
