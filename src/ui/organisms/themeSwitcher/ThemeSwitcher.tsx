4f75901d5b89c1ea7aabd44b83f799d6f8bc59af
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-12 18:15:25 +0100

    fix(theme): update theme switcher behaviour in order to handle system theme by default

--- src/ui/organisms/themeSwitcher/ThemeSwitcher.tsx
+++ src/ui/organisms/themeSwitcher/ThemeSwitcher.tsx
@@ -1,5 +1,3 @@
-import clsx from "clsx";
-
 import { MoonIcon } from "../../../assets/icons/MoonIcon";
 import { SunIcon } from "../../../assets/icons/SunIcon";
 
@@ -8,18 +6,24 @@ interface ThemeSwitcherProps {
   toggleDarkTheme: () => void;
 }
 
-export const ThemeSwitcher = ({ isDarkTheme, toggleDarkTheme }: ThemeSwitcherProps) => {
+export const ThemeSwitcher = ({ toggleDarkTheme }: ThemeSwitcherProps) => {
   return (
     <div className="flex justify-end relative z-10">
       <button
         className="flex items-center justify-end gap-3 text-xs h-8 p-0 border-none bg-transparent -translate-y-6"
         onClick={toggleDarkTheme}
       >
-        <span className="hidden sm:block">{`${isDarkTheme ? "DARK" : "LIGHT"}`}&nbsp;MODE&nbsp;ON</span>
+        <span className="hidden sm:block">
+          <span className="hidden dark:block">DARK&nbsp;MODE&nbsp;ON</span>
+          <span className="dark:hidden">LIGHT&nbsp;MODE&nbsp;ON</span>
+        </span>
         <div className="flex justify-start border-2 border-current rounded-2xl w-16 h-8">
-          <div className={clsx("w-1/2 transition-all duration-300", isDarkTheme && "translate-x-8")}>
-            <div className={clsx(isDarkTheme ? "text-white" : "text-gray-900")}>
-              {isDarkTheme ? <MoonIcon /> : <SunIcon />}
+          <div className="w-1/2 transition-all duration-300 dark:translate-x-8">
+            <div className="text-white hidden dark:block">
+              <MoonIcon />
+            </div>
+            <div className="text-gray-900 dark:hidden">
+              <SunIcon />
             </div>
           </div>
         </div>
