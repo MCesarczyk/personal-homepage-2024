c23ebdbc46b4cdd25e3a69938922f9bf96ce716e
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-13 17:10:59 +0100

    feat(skills): fetching unfiltered skills list in server component

--- src/app/admin/skills/page.tsx
+++ src/app/admin/skills/page.tsx
@@ -1,3 +1,36 @@
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { type SkillDto } from "@/app/admin/skills/types";
+
 export default async function SkillsPage() {
-  return <p className="py-8">Here you can manage your skills.</p>;
+  const accessToken = cookies().get("accessToken")?.value;
+
+  const response = await fetch(`${process.env.API_URL}${process.env.API_PREFIX}/skill`, {
+    method: "GET",
+    headers: {
+      "Content-Type": "application/json",
+      Authorization: `Bearer ${accessToken}`,
+    },
+    credentials: "include",
+  });
+
+  if (!response.ok) {
+    return redirect("/login");
+  }
+
+  const skills: SkillDto[] = (await response.json()) || [];
+
+  return (
+    <div className="py-8">
+      <ul>
+        {skills?.map(({ id, content, state }) => (
+          <li key={id}>
+            <a href={`/admin/skills/${id}`}>{content}</a>
+            <span>State: {state}</span>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
