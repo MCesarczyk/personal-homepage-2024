3f99b83c862e562275f47080adba12696060e5a2
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-18 16:33:47 +0100

    refactor(skills): use external library instead of local helper

--- src/app/admin/skills/page.tsx
+++ src/app/admin/skills/page.tsx
@@ -1,10 +1,10 @@
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 import * as jwt from "jsonwebtoken";
+import { sortArrayOfObjects } from "utils-agnostic";
 
 import { type SkillDto } from "@/app/admin/skills/types";
 import { Card } from "@/ui/molecules/card";
-import { sortArrayOfSkillsDescending } from "@/app/admin/skills/helpers";
 
 export default async function SkillsPage() {
   const accessToken = cookies().get("accessToken")?.value;
@@ -29,9 +29,8 @@ export default async function SkillsPage() {
   return (
     <div className="py-8">
       <ul className="flex flex-col gap-4">
-        {skills
+        {sortArrayOfObjects(skills, "state")
           ?.filter(({ userId }) => userId === currentUserId)
-          .sort(sortArrayOfSkillsDescending)
           .map(({ id, content, state }) => (
             <li key={id}>
               <Card content={content} state={state} />
