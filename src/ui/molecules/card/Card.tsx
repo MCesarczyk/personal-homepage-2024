7d6aab25cbc3da30a5df948cfcade803c6e3b07e
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-24 17:05:00 +0100

    feat(skill): add edit input to card

--- src/ui/molecules/card/Card.tsx
+++ src/ui/molecules/card/Card.tsx
@@ -1,8 +1,8 @@
 "use client";
 
-import { type ChangeEvent } from "react";
+import { useState, type ChangeEvent } from "react";
 import { type SkillDto, type SkillState } from "@/app/admin/skills/types";
-import { Select, Button } from "../../atoms";
+import { Select, Button, Input } from "../../atoms";
 
 interface CardProps {
   id: string;
@@ -13,19 +13,27 @@ interface CardProps {
   deleteSkill: (id: string) => void;
 }
 
-export const Card = ({ id, content, state, changeState, editSkill, deleteSkill }: CardProps) => {
+export const Card = ({ id, content, state, changeState, editSkill: _, deleteSkill }: CardProps) => {
+  const [isEditing, setIsEditing] = useState(false);
+
   const handleStateChange = (e: ChangeEvent<HTMLSelectElement>) =>
     changeState(id, { state: e.target.value as SkillState });
 
-  const handleEdit = () => editSkill(id);
+  const enableEditMode = () => setIsEditing(!isEditing);
+
+  const handleEditFinish = () => setIsEditing(false);
 
   const handleDelete = () => deleteSkill(id);
 
   return (
     <div className="text-left text-sm px-4 py-2 md:px-8 md:py-4 md:text-base rounded bg-white dark:bg-gray-800 border-4 border-gray-500 border-opacity-10 dark:border-opacity-10 shadow-md shadow-gray-900 transition-all ease-in duration-300 hover:shadow-blue-500 flex gap-4 md:gap-8 items-center justify-between">
-      <h3 className="text-base sm:text-lg md:text-xl lg:text-2xl font-bold text-blue-800 dark:text-blue-600">
-        {content}
-      </h3>
+      {isEditing ? (
+        <Input value={content} onBlur={handleEditFinish} />
+      ) : (
+        <h3 className="text-base sm:text-lg md:text-xl lg:text-2xl font-bold text-blue-800 dark:text-blue-600">
+          {content}
+        </h3>
+      )}
       <div className="flex gap-4 md:gap-8">
         <Select
           options={[
@@ -36,7 +44,7 @@ export const Card = ({ id, content, state, changeState, editSkill, deleteSkill }
           value={state}
           onChange={handleStateChange}
         />
-        <Button onClick={handleEdit} variant="SECONDARY">
+        <Button onClick={enableEditMode} variant="SECONDARY">
           Edit
         </Button>
         <Button onClick={handleDelete} variant="SECONDARY">
