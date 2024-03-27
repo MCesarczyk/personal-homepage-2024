18d24ef10d234a9b1997f102555900819e5a732c
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2024-03-27 07:05:40 +0100

    fix: update link and button on main page

--- src/ui/organisms/header/Header.tsx
+++ src/ui/organisms/header/Header.tsx
@@ -16,7 +16,7 @@ export const Header = ({ name, description, Portrait }: HeaderProps) => (
       <Caption>THIS IS:</Caption>
       <PageTitle>{name}</PageTitle>
       <p className="mb-8">{description}</p>
-      <Link href="#contact">
+      <Link variant="PRIMARY" href="#contact">
         <div className="w-6 h-5 mr-3 sm:mr-4 md:mr-5 sm:scale-110 md:scale-125">
           <EnvelopeIcon />
         </div>
